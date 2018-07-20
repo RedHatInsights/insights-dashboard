d275ea947f2d961ed18aceb59294617db24df884
Ryan Long <rlong@redhat.com>
2018-07-20 14:34:16 -0400

    restructuring travis for PRs

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 # Check if it is a pull request
+# If it is not a pull request, generate the deploy key
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     echo -e "Pull Request, not pushing a build"
     exit 0;
+else
+    openssl aes-256-cbc -K $encrypted_8e579b44016d_key -iv $encrypted_8e579b44016d_iv -in deploy_key.enc -out deploy_key -d
+    chmod 600 deploy_key
+    eval `ssh-agent -s`
+    ssh-add deploy_key
 fi
 
 # If current dev branch is master, push to build repo master
