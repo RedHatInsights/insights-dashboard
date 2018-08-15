98c993cd94682b965cf9a94235f2b3353bbd059b
Josh Kinlaw <jkinlaw@redhat.com>
2018-08-14 21:14:29 -0400

    adds grid card layout

--- src/SmartComponents/SamplePage/SamplePage.js
+++ src/SmartComponents/SamplePage/SamplePage.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import asyncComponent from '../../Utilities/asyncComponent';
 import './sample-page.scss';
 
-import { PageHeader } from '@red-hat-insights/insights-frontend-components';
-import { PageHeaderTitle } from '@red-hat-insights/insights-frontend-components';
-import { Section } from '@red-hat-insights/insights-frontend-components';
-
-import { Button } from '@patternfly/react-core';
-
-const SampleComponent = asyncComponent(() => import('../../PresentationalComponents/SampleComponent/sample-component'));
+import { Grid, GridItem } from '@patternfly/react-core';
 
 /**
  * A smart component that handles all the api calls and data needed by the dumb components.
@@ -22,23 +15,10 @@ class SamplePage extends Component {
 
     render() {
         return (
-            <React.Fragment>
-                <PageHeader>
-                    <PageHeaderTitle title='Sample Insights App'/>
-                </PageHeader>
-                <Section type='content'>
-                    <h1> Sample Component </h1>
-                    <SampleComponent> Sample Component </SampleComponent>
-                    <h1> Cards </h1>
-                    <h1> Buttons </h1>
-                    <Section type='button-group'>
-                        <Button variant='primary'> PF-Next Primary Button </Button>
-                        <Button variant='secondary'> PF-Next Secondary Button </Button>
-                        <Button variant='tertiary'> PF-Next Tertiary Button </Button>
-                        <Button variant='danger'> PF-Next Danger Button </Button>
-                    </Section>
-                </Section>
-            </React.Fragment>
+            <Grid>
+                <GridItem span={4}>Test</GridItem>
+                <GridItem span={4}>Tester</GridItem>
+            </Grid>
         );
     }
 }
