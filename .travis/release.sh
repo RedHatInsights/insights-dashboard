e89399a48fd8b4a7fb20ce4d455926772b368e95
Chris Hambridge <chambrid@redhat.com>
2018-10-17 10:52:24 -0400

    updates deployments to support releasing to pre-prod environments

--- .travis/release.sh
+++ .travis/release.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-npm run verify:beta
-git clone ${REPO}.git
-cd ${REPO_DIR}
-cp Jenkinsfile ../dist/Jenkinsfile
-cd ../dist
+git clone ${REPO}.git -b $1
+cd dist
+cp ../${REPO_DIR}/Jenkinsfile ./Jenkinsfile
 git init
 git config --global user.name $COMMIT_AUTHOR_USERNAME
 git config --global user.email $COMMIT_AUTHOR_EMAIL
 git remote add travis-build ${REPO}.git
 git add .
 git commit -m "${TRAVIS_COMMIT_MESSAGE}"
-git push --force --set-upstream travis-build master
+git push --force --set-upstream travis-build HEAD:$1
