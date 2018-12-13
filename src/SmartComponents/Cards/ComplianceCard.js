919ef941ae7f70735f42b8fc4ff688f74a77a69f
Josh <jkinlaw@redhat.com>
2018-12-13 09:26:06 -0500

    adds api data to compliance card

--- src/SmartComponents/Cards/ComplianceCard.js
+++ src/SmartComponents/Cards/ComplianceCard.js
@@ -35,34 +35,48 @@ class ComplianceCard extends Component {
     }
 
     render() {
+        const {
+            complianceFetchStatus,
+            complianceSummary
+        } = this.props;
+
         return (
             <Card className='pf-m-dark'>
                 <CardHeader>
                     <Title className="pf-u-mt-0 pf-u-mb-0" size={'lg'}>Compliance</Title>
                 </CardHeader>
                 <CardBody>
-                    <GaugeWidget label='compliance' width={250} height={250} value={42}
-                        identifier='compliance-gauge' negative flipFullColors
-                        changeValue='4' timeframe='30' className='pf-m-gutter'>
-                        <ul className='ins-c-gauge-widget__legend-list'>
-                            <li className='ins-c-gauge-widget__legend-list-item ins-m-special'>
-                                <span className='ins-c-gauge-widget__legend-list-count ins-m-emphasis'>10</span>
-                                <span className='ins-c-gauge-widget__legend-list-type ins-m-dark'>New</span>
-                            </li>
-                            <li className='ins-c-gauge-widget__legend-list-item'>
-                                <span className='ins-c-gauge-widget__legend-list-count'>7</span>
-                                <span className='ins-c-gauge-widget__legend-list-type'>Critical</span>
-                            </li>
-                            <li className='ins-c-gauge-widget__legend-list-item'>
-                                <span className='ins-c-gauge-widget__legend-list-count'>3</span>
-                                <span className='ins-c-gauge-widget__legend-list-type'>Warning</span>
-                            </li>
-                        </ul>
-                        <a className='ins-c-icon-inline' href={'/' + release + 'insights/platform/compliance/'}>
-                            <span>View All</span>
-                            <i className='fas fa-external-link-alt'/>
-                        </a>
-                    </GaugeWidget>
+
+                    { complianceFetchStatus === 'fulfilled' && (
+                        <ul>
+                            { complianceSummary.map((profile, key) => <li> {profile.attributes.name}: {score * 100}% compliance</li> ) }
+                    ) }
+
+                    { complianceFetchStatus === 'pending' && (<Loading /> ) }
+                    
+
+                    // <GaugeWidget label='compliance' width={250} height={250} value={42}
+                    //     identifier='compliance-gauge' negative flipFullColors
+                    //     changeValue='4' timeframe='30' className='pf-m-gutter'>
+                    //     <ul className='ins-c-gauge-widget__legend-list'>
+                    //         <li className='ins-c-gauge-widget__legend-list-item ins-m-special'>
+                    //             <span className='ins-c-gauge-widget__legend-list-count ins-m-emphasis'>10</span>
+                    //             <span className='ins-c-gauge-widget__legend-list-type ins-m-dark'>New</span>
+                    //         </li>
+                    //         <li className='ins-c-gauge-widget__legend-list-item'>
+                    //             <span className='ins-c-gauge-widget__legend-list-count'>7</span>
+                    //             <span className='ins-c-gauge-widget__legend-list-type'>Critical</span>
+                    //         </li>
+                    //         <li className='ins-c-gauge-widget__legend-list-item'>
+                    //             <span className='ins-c-gauge-widget__legend-list-count'>3</span>
+                    //             <span className='ins-c-gauge-widget__legend-list-type'>Warning</span>
+                    //         </li>
+                    //     </ul>
+                    //     <a className='ins-c-icon-inline' href={'/' + release + 'insights/platform/compliance/'}>
+                    //         <span>View All</span>
+                    //         <i className='fas fa-external-link-alt'/>
+                    //     </a>
+                    // </GaugeWidget>
                 </CardBody>
             </Card>
         );
