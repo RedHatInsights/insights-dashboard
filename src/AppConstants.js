e5e5b89ab2512af54f77a35aaa87841b70bd9584
Josh <jkinlaw@redhat.com>
2018-12-21 09:41:14 -0500

    fixes ocp summary url

--- src/AppConstants.js
+++ src/AppConstants.js
@@ -4,6 +4,5 @@ export const OCP_SUMMARY_FETCH = 'OCP_SUMMARY_FETCH';
 const BASE_URL = '/r/insights/platform';
 
 export const COMPLIANCE_FETCH_URL = `${BASE_URL}/compliance/profiles`;
-export const OCP_SUMMARY_FETCH_URL = `${BASE_URL}/https://api.access.ci.itop.redhat.com/r/insights/platform/\
-									  cost-management/api/v1/reports/charges/ocp/?filter[time_scope_units]=mon\
+export const OCP_SUMMARY_FETCH_URL = `${BASE_URL}/cost-management/api/v1/reports/charges/ocp/?filter[time_scope_units]=mon
 									  th&filter[time_scope_value]=-1&filter[resolution]=monthly&delta=charge`;
