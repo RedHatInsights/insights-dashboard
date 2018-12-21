4145555ce12a4288e3b0016d289e32ac97a4f0ad
Josh <jkinlaw@redhat.com>
2018-12-21 09:51:08 -0500

    fixing ocp url

--- src/AppConstants.js
+++ src/AppConstants.js
@@ -5,4 +5,4 @@ const BASE_URL = '/r/insights/platform';
 
 export const COMPLIANCE_FETCH_URL = `${BASE_URL}/compliance/profiles`;
 export const OCP_SUMMARY_FETCH_URL = `${BASE_URL}/cost-management/api/v1/reports/charges/ocp/?filter[time_scope_units]=mon
-									  th&filter[time_scope_value]=-1&filter[resolution]=monthly&delta=charge`;
+									th&filter[time_scope_value]=-1&filter[resolution]=monthly&delta=charge`;
