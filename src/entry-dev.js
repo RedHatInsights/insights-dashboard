3e71d9144ae537780453850c2d527403a256b448
Allen Wight <awight@redhat.com>
2020-02-20 15:14:26 -0500

    Adds support for internationalization (#52)

--- src/entry-dev.js
+++ src/entry-dev.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-console */
 import App from './App';
+import { IntlProvider } from '@redhat-cloud-services/frontend-components-translations';
 import { NotificationsPortal } from '@redhat-cloud-services/frontend-components-notifications';
 import { Provider } from 'react-redux';
 import React from 'react';
@@ -7,18 +9,21 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { getBaseName } from '@redhat-cloud-services/frontend-components-utilities/files/helpers';
 import { init } from './Store';
 import logger from 'redux-logger';
+import messages from '../locales/data.json';
 
 const rootEl = document.getElementById('root');
 
 ReactDOM.render(
-    <Provider store={ init(logger).getStore() }>
-        <Router basename={ getBaseName(window.location.pathname) }>
-            <React.Fragment>
-                <NotificationsPortal />
-                <App />
-            </React.Fragment>
-        </Router>
-    </Provider>,
+    <IntlProvider locale={ navigator.language } messages={ messages } onError={ console.log }>
+        <Provider store={ init(logger).getStore() }>
+            <Router basename={ getBaseName(window.location.pathname) }>
+                <React.Fragment>
+                    <NotificationsPortal />
+                    <App />
+                </React.Fragment>
+            </Router>
+        </Provider>
+    </IntlProvider>,
 
     rootEl,
     () => rootEl.setAttribute('data-ouia-safe', true)
--- src/entry.js
+++ src/entry.js
@@ -1,4 +1,5 @@
 import App from './App';
+import { IntlProvider } from '@redhat-cloud-services/frontend-components-translations';
 import { NotificationsPortal } from '@redhat-cloud-services/frontend-components-notifications';
 import { Provider } from 'react-redux';
 import React from 'react';
@@ -6,18 +7,21 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { getBaseName } from '@redhat-cloud-services/frontend-components-utilities/files/helpers';
 import { init } from './Store';
+import messages from '../locales/data.json';
 
 const rootEl = document.getElementById('root');
 
 ReactDOM.render(
-    <Provider store={ init().getStore() }>
-        <Router basename={ getBaseName(window.location.pathname) }>
-            <React.Fragment>
-                <NotificationsPortal />
-                <App />
-            </React.Fragment>
-        </Router>
-    </Provider>,
+    <IntlProvider locale={ navigator.language } messages={ messages } onError={ console.log }>
+        <Provider store={ init().getStore() }>
+            <Router basename={ getBaseName(window.location.pathname) }>
+                <React.Fragment>
+                    <NotificationsPortal />
+                    <App />
+                </React.Fragment>
+            </Router>
+        </Provider>
+    </IntlProvider>,
 
     rootEl,
     () => rootEl.setAttribute('data-ouia-safe', true)
