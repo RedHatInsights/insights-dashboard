1192c9f6a2d4cbc1d934fd62f585cf5ee3ebf041
Christie Molloy <cmolloy@redhat.com>
2020-03-19 17:05:23 -0400

    work on template

--- src/SmartComponents/Advisor/Advisor.js
+++ src/SmartComponents/Advisor/Advisor.js
@@ -7,12 +7,14 @@ import React, { useEffect } from 'react';
 import { TemplateCard, TemplateCardBody, TemplateCardHeader } from '../../PresentationalComponents/Template/TemplateCard';
 import Loading from '../../PresentationalComponents/Loading/Loading';
 import PropTypes from 'prop-types';
-import StackChart from './StackChart';
 import { UI_BASE } from '../../AppConstants';
 import { connect } from 'react-redux';
 import { injectIntl } from 'react-intl';
 import messages from '../../Messages';
 import { NumberDescription } from '../../PresentationalComponents/NumberDescription/NumberDescription';
+import StackChartTemplate from '../../ChartTemplates/StackChart/StackChartTemplate';
+import { SEVERITY_MAP } from './Constants';
+import { capitalize } from '../../Utilities/Common';
 
 /**
  * Advisor Card for showing count/severity of rec hits
@@ -25,6 +27,16 @@ const Advisor = ({ recStats, recStatsStatus, advisorFetchStatsRecs, advisorFetch
         advisorFetchStatsSystems();
         advisorFetchIncidents();
     }, [advisorFetchIncidents, advisorFetchStatsRecs, advisorFetchStatsSystems]);
+
+    const stackChartPadding = { bottom: 0, left: 0, right: 0, top: 0 };
+    const legendClick = () => [{
+        target: 'labels',
+        mutation: (data) => {
+            const risk = data.datum.name.split(' ')[1].toLowerCase();
+            window.location.href =
+                `${UI_BASE}/advisor/recommendations?total_risk=${SEVERITY_MAP[risk]}&reports_shown=true&impacting=true&offset=0&limit=10`;
+        }
+    }];
 
     return <TemplateCard appName='Advisor'>
         <TemplateCardHeader title='Advisor recommendations' />
@@ -39,7 +51,21 @@ const Advisor = ({ recStats, recStatsStatus, advisorFetchStatsRecs, advisorFetch
                     link={ `${UI_BASE}${INCIDENT_URL}` }
                 />
             }
-            {recStatsStatus !== 'fulfilled' ? <Loading /> : <StackChart data={ recStats.total_risk } />}
+            {recStatsStatus !== 'fulfilled' ? <Loading /> :
+                <StackChartTemplate
+                    ariaDesc="CVEs impacting your systems"
+                    ariaTitle="Vulnerabilities chart"
+                    padding={ stackChartPadding }
+                    legendPosition="bottom-left"
+                    height={ 40 }
+                    width={ 600 }
+                    maxWidth={ 600 }
+                    legendHeight={ 36 }
+                    legendWidth={ 600 }
+                    data={ recStats.total_risk }
+                    legendClick={ legendClick }
+                />
+            }
             {systemsStatsStatus !== 'fulfilled' ? <Loading /> :
                 <React.Fragment>
                     <a href={ `${UI_BASE}${NEW_REC_URL}` }>
