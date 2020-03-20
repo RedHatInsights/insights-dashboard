83e903dc178420166542b1eefb47ba013540b630
Christie Molloy <cmolloy@redhat.com>
2020-03-20 10:56:57 -0400

    fix(compliance): update text and add links

--- src/SmartComponents/Compliance/ComplianceCard.js
+++ src/SmartComponents/Compliance/ComplianceCard.js
@@ -1,6 +1,7 @@
 import * as AppActions from '../../AppActions';
 import React, { Component } from 'react';
 import { EmptyState, EmptyStateVariant } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyState';
+import { EmptyStateSecondaryActions } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateSecondaryActions';
 import { Stack } from '@patternfly/react-core/dist/js/layouts/Stack/Stack';
 import { StackItem } from '@patternfly/react-core/dist/js/layouts/Stack/StackItem';
 import { Split } from '@patternfly/react-core/dist/js/layouts/Split/Split';
@@ -150,15 +151,26 @@ class ComplianceCard extends Component {
                                     <EmptyState className="ins-c-compliance__empty-state" variant={ EmptyStateVariant.full }>
                                         <TextContent>
                                             <Text component={ TextVariants.p }>
-                                                No policies are reporting
+                                                No policies
                                             </Text>
                                             <Text component={ TextVariants.small }>
-                                                The compliance service uses SCAP policies to track your organization&apos;s
-                                                adherence to compliance requirements.
+                                                The compliance service uses OpenSCAP policies to track your organization&apos;s
+                                                adherence to compliance requirements
                                             </Text>
-                                            <Text component={ TextVariants.small }>
-                                                <a href={ `${UI_BASE}/compliance/policies/` }>Learn about OpenSCAP and Compliance</a>
-                                            </Text>
+                                            <EmptyStateSecondaryActions>
+                                                <Button
+                                                    variant="link"
+                                                    href={ `${UI_BASE}/compliance/policies/` }
+                                                >
+                                                    Create a policy
+                                                </Button>
+                                                <Button
+                                                    variant="link"
+                                                    href="https://www.open-scap.org/getting-started/"
+                                                >
+                                                    Learn about OpenSCAP
+                                                </Button>
+                                            </EmptyStateSecondaryActions>
                                         </TextContent>
                                     </EmptyState>
                                 ))
