b2af003a677a132a38568e58b4af906e850151bf
Christie Molloy <cmolloy@redhat.com>
2020-03-26 16:44:37 -0400

    systemInventoryCard update template

---
+++ src/Icons/InfoIcon.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import InfoCircleIcon from '@patternfly/react-icons/dist/js/icons/info-circle-icon';
+import './Icons.scss';
+
+const InfoIcon = () => <InfoCircleIcon className="ins-c-dashboard__icon-info"/>;
+
+export default InfoIcon;
--- src/PresentationalComponents/NumberData/NumberData.js
+++ src/PresentationalComponents/NumberData/NumberData.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import '../../../src/PresentationalComponents/NumberData/NumberData.scss';
+import InfoIcon from '../../Icons/InfoIcon';
 
-export const NumberData = ({ data, dataSize }) => (
-    <p className={ `ins-c-dashboard__number-data--${dataSize}` }>
-        {data}
-    </p>
+export const NumberData = ({ data, dataSize, percentageData }) => (
+    <div className="ins-c-dashboard__number-data--number-percentage">
+        <span className={ `ins-c-dashboard__number-data--${dataSize}` }>
+            {data}
+        </span>
+        { percentageData &&
+            <span className="ins-c-dashboard__number-percentage">
+                <span>{percentageData}</span>
+                <InfoIcon/>
+            </span>
+        }
+    </div>
 );
 
 NumberData.propTypes = {
     data: propTypes.any,
     dataSize: propTypes.string,
-    linkDescription: propTypes.string
+    linkDescription: propTypes.string,
+    percentageData: propTypes.string
 };
 
 export default NumberData;
