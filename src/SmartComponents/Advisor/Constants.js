c2d867637827a0d5ad7a8b2862d5aecbacd42acd
Allen Wight <awight@redhat.com>
2020-03-27 14:55:02 -0400

    For Brandon - Show href go to advisor table with impacting ☕️💯

--- src/SmartComponents/Advisor/Constants.js
+++ src/SmartComponents/Advisor/Constants.js
@@ -1,5 +1,5 @@
 export const INCIDENT_URL = `/advisor/recommendations?reports_shown=undefined&impacting=false&offset=0&limit=10&sort=-publish_date&incident=true`;
-export const NEW_REC_URL = `/advisor/recommendations?reports_shown=undefined&impacting=false&offset=0&limit=10&sort=-publish_date`;
+export const NEW_REC_URL = `/advisor/recommendations?reports_shown=undefined&impacting=true&offset=0&limit=10&sort=-publish_date`;
 
 export const SEVERITY_MAP = {
     critical: 4,
