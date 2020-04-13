7b24a325999b03baf62c69d3b960de2a6fd42697
Allen Wight <awight@redhat.com>
2020-04-13 13:49:44 -0400

    fiddles around with run status to keep it tight n working

--- src/SmartComponents/Remediations/RunStatus.js
+++ src/SmartComponents/Remediations/RunStatus.js
@@ -1,91 +1,64 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import * as AppActions from '../../AppActions';
-import routerParams from '@redhat-cloud-services/frontend-components-utilities/files/RouterParams';
-import FinishedIcon from './../../Icons/FinishedIcon';
-import RunningIcon from './../../Icons/RunningIcon';
-import QuestionIcon from './../../Icons/QuestionIcon';
-import { Button } from '@patternfly/react-core/dist/js/components/Button/Button';
-import TimeStamp from './../../PresentationalComponents/TimeStamp/TimeStamp';
-import { DateFormat } from '@redhat-cloud-services/frontend-components';
-import { Skeleton } from '@red-hat-insights/insights-frontend-components';
+import React, { useEffect, useState } from 'react';
 
-const RunStatus = ({
-    id,
-    name,
-    fetchRemediationsPlaybookRuns,
-    // remediationsPlaybookRunsFetchStatus,
-    remediationsPlaybookRuns
-}) => {
+import API from '../../Utilities/Api';
+import { Button } from '@patternfly/react-core/dist/js/components/Button/Button';
+import { DateFormat } from '@redhat-cloud-services/frontend-components';
+import FinishedIcon from './../../Icons/FinishedIcon';
+import PropTypes from 'prop-types';
+import QuestionIcon from './../../Icons/QuestionIcon';
+import { REMEDIATIONS_PLAYBOOK_RUNS_FETCH_URL } from '../../AppConstants';
+import RunningIcon from './../../Icons/RunningIcon';
+import { Skeleton } from '@red-hat-insights/insights-frontend-components';
+import TimeStamp from './../../PresentationalComponents/TimeStamp/TimeStamp';
+
+const RunStatus = ({ id, name }) => {
+    const [playbookRun, setPlaybookRun] = useState({});
+    const [loaded, setLoaded] = useState();
 
     useEffect(() => {
-        fetchRemediationsPlaybookRuns(id);
-    }, [fetchRemediationsPlaybookRuns]);
+        const fetchPlaybookRun = async () => {
+            try {
+                const response = await API.get(`${REMEDIATIONS_PLAYBOOK_RUNS_FETCH_URL}${id}/playbook_runs?limit=1&offset=0&sort=-updated_at`);
+                setPlaybookRun(response.data.data);
+                setLoaded(true);
+                console.error(response.data);
+            } catch (error) {
+                setLoaded(false);
+                console.error('oopsie');
+            }
+        };
 
-    console.log(remediationsPlaybookRuns.data);
+        fetchPlaybookRun();
+    }, [id]);
 
-    if (remediationsPlaybookRuns.data) {
-        return (
-            remediationsPlaybookRuns.data && remediationsPlaybookRuns.data.length ?
-                <div className="ins-c-remediations-container">
-                    <div className="ins-c-remediation__status">
-                        <React.Fragment>
-                            { remediationsPlaybookRuns.data[0].status === 'Running' ? (
-                                <RunningIcon/>
-                            ) : (
-                                <FinishedIcon/>
-                            )}
-                            <p>{remediationsPlaybookRuns.data[0].status}</p>
-                        </React.Fragment>
-                    </div>
-                    <div className="ins-c-remediation__timestamp">
-                        <Button component="a" variant="link" isInline href={ `./insights/remediations/${id}` }>
-                            { name }
-                        </Button>
-                        <TimeStamp timestamp={ <DateFormat type='exact' date={ remediationsPlaybookRuns.data[0].created_at }/> }/>
-                    </div>
-                </div>
-                : <div className="ins-c-remediations-container">
-                    <div className="ins-c-remediation__status">
-                        <React.Fragment>
-                            <QuestionIcon/>
-                            <p>No run data</p>
-                        </React.Fragment>
-                    </div>
-                    <div className="ins-c-remediation__timestamp">
-                        <Button component="a" variant="link" isInline href={ `./insights/remediations/${id}` }>
-                            { name }
-                        </Button>
-                        <TimeStamp timestamp='none'/>
-                    </div>
-                </div>
-        );
-    } else {
-        return <Skeleton size='md'/>;
-    }
+    return <div className="ins-c-remediations-container">
+        <div className="ins-c-remediation__status">
+            {loaded === undefined && <Skeleton size='md' />}
+            {loaded ? <React.Fragment>
+                {playbookRun[0].status === 'Running' ? <RunningIcon /> : <FinishedIcon />}
+                <p>{playbookRun[0].status}</p>
+            </React.Fragment>
+                : <React.Fragment>
+                    <QuestionIcon />
+                    <p>No run data</p>
+                </React.Fragment>
+            }
+        </div>
+        <div className="ins-c-remediation__timestamp">
+            <Button component="a" variant="link" isInline href={ `./insights/remediations/${id}` }>
+                {name}
+            </Button>
+            {loaded === undefined ? <Skeleton size='md' /> :
+                loaded && <TimeStamp timestamp={ <DateFormat type='exact' date={ playbookRun[0].created_at } /> } />
+            }
+        </div>
+    </div>;
 };
 
 RunStatus.propTypes = {
     id: PropTypes.string,
     name: PropTypes.string,
-    fetchRemediationsPlaybookRuns: PropTypes.func,
-    remediationsPlaybookRuns: PropTypes.object,
-    remediationsPlaybookRunsFetchStatus: PropTypes.string,
     intl: PropTypes.any
 };
 
-const mapStateToProps = (state, ownProps) => ({
-    remediationsPlaybookRuns: state.DashboardStore.remediationsPlaybookRuns,
-    remediationsPlaybookRunsFetchStatus: state.DashboardStore.remediationsPlaybookRunsFetchStatus,
-    ...ownProps
-});
-
-const mapDispatchToProps = dispatch => ({
-    fetchRemediationsPlaybookRuns: (id) => dispatch(AppActions.fetchRemediationsPlaybookRuns(id))
-});
-
-export default routerParams(connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(RunStatus));
+export default RunStatus;
