8158b6786e96528c12190a8409ab893fe9f2d85f
Ryan Long <rlong@redhat.com>
2020-04-14 14:39:20 -0400

    remove icon from empty state

--- src/SmartComponents/Remediations/RemediationsEmptyState.js
+++ src/SmartComponents/Remediations/RemediationsEmptyState.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 import { EmptyState, EmptyStateVariant } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyState';
-import { EmptyStateIcon } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateIcon';
 import { EmptyStateBody } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateBody';
 import { Title } from '@patternfly/react-core/dist/js/components/Title/Title';
 import { Bullseye } from '@patternfly/react-core/dist/js/layouts/Bullseye/Bullseye';
 
-import WrenchIcon from '@patternfly/react-icons/dist/js/icons/wrench-icon';
 import PropTypes from 'prop-types';
 
 import { useIntl } from 'react-intl';
@@ -19,7 +17,6 @@ export const RemediationsEmptyState = () => {
     return (
         <Bullseye>
             <EmptyState variant={ EmptyStateVariant.small }>
-                <EmptyStateIcon icon={ WrenchIcon }/>
                 <Title headingLevel="h6" size="md">
                     { intl.formatMessage(messages.remediationsNoDataTitle)}
                 </Title>
