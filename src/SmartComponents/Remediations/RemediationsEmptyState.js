3c2fc84d997427548642fe4c2e3764d59cb3060c
Ryan Long <rlong@redhat.com>
2020-04-14 14:42:21 -0400

    fix styling

--- src/SmartComponents/Remediations/RemediationsEmptyState.js
+++ src/SmartComponents/Remediations/RemediationsEmptyState.js
@@ -3,28 +3,27 @@ import React from 'react';
 import { EmptyState, EmptyStateVariant } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyState';
 import { EmptyStateBody } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateBody';
 import { Title } from '@patternfly/react-core/dist/js/components/Title/Title';
-import { Bullseye } from '@patternfly/react-core/dist/js/layouts/Bullseye/Bullseye';
 
 import PropTypes from 'prop-types';
 
 import { useIntl } from 'react-intl';
 import messages from '../../Messages';
 
+import './RemediationsEmptyState.scss';
+
 export const RemediationsEmptyState = () => {
 
     const intl = useIntl();
 
     return (
-        <Bullseye>
-            <EmptyState variant={ EmptyStateVariant.small }>
-                <Title headingLevel="h6" size="md">
-                    { intl.formatMessage(messages.remediationsNoDataTitle)}
-                </Title>
-                <EmptyStateBody>
-                    { intl.formatMessage(messages.remediationsNoDataBody)}
-                </EmptyStateBody>
-            </EmptyState>
-        </Bullseye>
+        <EmptyState variant={ EmptyStateVariant.small }>
+            <Title headingLevel="h6" size="md">
+                { intl.formatMessage(messages.remediationsNoDataTitle)}
+            </Title>
+            <EmptyStateBody>
+                { intl.formatMessage(messages.remediationsNoDataBody)}
+            </EmptyStateBody>
+        </EmptyState>
     );
 };
 
