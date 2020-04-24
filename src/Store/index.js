74bc620f40db6b5ef5e40327f928b6dcdd854b1b
Ryan Long <rlong@redhat.com>
2020-04-23 21:01:07 -0400

    remove notifications portal

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,5 +1,3 @@
-import { notifications, notificationsMiddleware } from '@redhat-cloud-services/frontend-components-notifications';
-
 import { DashboardStore } from '../AppReducer';
 import { compose } from 'redux';
 import { getRegistry } from '@redhat-cloud-services/frontend-components-utilities/files/Registry';
@@ -11,13 +9,10 @@ export const init = (...middleware) => {
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     registry = getRegistry(
         {},
-        [...middleware, promiseMiddleware, notificationsMiddleware({
-            errorTitleKey: ['message']
-        })],
+        [...middleware, promiseMiddleware],
         composeEnhancers
     );
     registry.register({ DashboardStore });
-    registry.register({ notifications });
 
     return registry;
 };
