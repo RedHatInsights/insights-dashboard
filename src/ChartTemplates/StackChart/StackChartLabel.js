537527564638af3a0be90941b8f3634cfcc39c53
Christie Molloy <cmolloy@redhat.com>
2020-05-12 15:20:23 -0400

    fix links

--- src/ChartTemplates/StackChart/StackChartLabel.js
+++ src/ChartTemplates/StackChart/StackChartLabel.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { ChartLabel } from '@patternfly/react-charts';
 import propTypes from 'prop-types';
+import './_StackChartTemplate.scss';
 
 export const StackChartLabel = ({ link, ...props }) => (
-    <a className="pf-c-button pf-m-link" href={ link[props.index] }><ChartLabel { ...props }/></a>
+    <a className="pf-c-button pf-m-link pf-m-inline ins-chart-label" href={ link[props.index] }><ChartLabel { ...props }/></a>
 );
 
 StackChartLabel.propTypes = {
