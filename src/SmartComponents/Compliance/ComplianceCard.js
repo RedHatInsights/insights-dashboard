afce43b1a4430d7ff5df4f3c6272612578065595
Allen Wight <awight@redhat.com>
2020-06-23 07:59:04 -0400

    Updates deps, makes compliance links accessible by pendo

--- src/SmartComponents/Compliance/ComplianceCard.js
+++ src/SmartComponents/Compliance/ComplianceCard.js
@@ -1,37 +1,28 @@
+import './ComplianceCard.scss';
+
 /* eslint-disable camelcase */
 import * as AppActions from '../../AppActions';
 
 import { EmptyState, EmptyStateVariant } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyState';
 import React, { useEffect } from 'react';
-import {
-    TemplateCard,
-    TemplateCardBody,
-    TemplateCardHeader
-} from '../../PresentationalComponents/Template/TemplateCard';
+import { TemplateCard, TemplateCardBody, TemplateCardHeader } from '../../PresentationalComponents/Template/TemplateCard';
+import { chart_color_blue_200, chart_color_blue_300 } from '@patternfly/react-tokens';
 
 import { Button } from '@patternfly/react-core/dist/js/components/Button/Button';
 import { EmptyStateBody } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateBody';
 import { EmptyStateSecondaryActions } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateSecondaryActions';
+import FailState from '../../PresentationalComponents/FailState/FailState';
 import Loading from '../../PresentationalComponents/Loading/Loading';
 import { PieChart } from '../../ChartTemplates/PieChart/PieChartTemplate';
 import PropTypes from 'prop-types';
 import { Split } from '@patternfly/react-core/dist/js/layouts/Split/Split';
 import { SplitItem } from '@patternfly/react-core/dist/js/layouts/Split/SplitItem';
-import { Stack } from '@patternfly/react-core/dist/js/layouts/Stack/Stack';
-import { StackItem } from '@patternfly/react-core/dist/js/layouts/Stack/StackItem';
 import { Title } from '@patternfly/react-core/dist/js/components/Title/Title';
 import { UI_BASE } from '../../AppConstants';
 import { connect } from 'react-redux';
-import { useIntl } from 'react-intl';
 import messages from '../../Messages';
 import routerParams from '@redhat-cloud-services/frontend-components-utilities/files/RouterParams';
-import './ComplianceCard.scss';
-import {
-    chart_color_blue_200,
-    chart_color_blue_300
-} from '@patternfly/react-tokens';
-
-import FailState from '../../PresentationalComponents/FailState/FailState';
+import { useIntl } from 'react-intl';
 
 const ComplianceCard = ({ fetchCompliance, complianceFetchStatus, complianceSummary }) => {
 
@@ -65,116 +56,112 @@ const ComplianceCard = ({ fetchCompliance, complianceFetchStatus, complianceSumm
         >
             <TemplateCardHeader title='Compliance' />
             <TemplateCardBody>
-                <Stack>
-                    {complianceFetchStatus === 'fulfilled' &&
-                        (Array.isArray(complianceSummary.data) &&
-                            (complianceSummary.data.length > 0 ? <React.Fragment>
-                                {getTopThreePolicies(complianceSummary).map((policy, index) =>
-                                    <div className="ins-c-compliance__row" key={ index }>
-                                        <div className="ins-c-compliance__row-item">
-                                            <PieChart
-                                                ariaDesc="Compliance score"
-                                                ariaTitle="Pie chart compliance"
-                                                constrainToVisibleArea={ true }
-                                                data={ [
-                                                    {
-                                                        x: 'Compliant',
-                                                        y: policy.attributes.compliant_host_count
-                                                    }, {
-                                                        x: 'Non-compliant',
-                                                        y: policy.attributes.total_host_count - policy.attributes.compliant_host_count
-                                                    }
-                                                ] }
-                                                labels={ ({ datum }) => `${datum.x}: ${datum.y}` }
-                                                padding={ pieChartPadding }
-                                                height={ 65 }
-                                                width={ 65 }
-                                                colorScale={ colorScale }
-                                            />
-                                        </div>
-                                        <div className="ins-c-compliance__row-item">
-                                            <Stack>
-                                                <StackItem>
-                                                    <Button
-                                                        className="ins-c-compliance__policy-link"
-                                                        component="a"
-                                                        href={ `/${UI_BASE}/compliance/reports/${policy.id}` }
-                                                        variant="link"
-                                                        isInline
-                                                    >
-                                                        {policy.attributes.name}
-                                                    </Button>
-                                                </StackItem>
-                                                <StackItem>
-                                                    <Split gutter='sm'>
-                                                        <SplitItem>
-                                                            {intl.formatMessage(messages.compliantHostCount,
-                                                                { count: policy.attributes.total_host_count }
-                                                            )}
-                                                        </SplitItem>
-                                                        <SplitItem>
-                                                            {intl.formatMessage(messages.compliantScore,
-                                                                { score: Math.trunc(
-                                                                    policy.attributes.compliant_host_count / policy.attributes.total_host_count
-                                                                ) }
-                                                            )}
-                                                        </SplitItem>
-                                                    </Split>
-                                                </StackItem>
-                                            </Stack>
-                                        </div>
-                                    </div>
-                                )}
-                                <div className="ins-c-compliance__row">
+                {complianceFetchStatus === 'fulfilled' &&
+                    (Array.isArray(complianceSummary.data) &&
+                        (complianceSummary.data.length > 0 ? <React.Fragment>
+                            {getTopThreePolicies(complianceSummary).map((policy, index) =>
+                                <div className="ins-c-compliance__row" key={ index }>
                                     <div className="ins-c-compliance__row-item">
+                                        <PieChart
+                                            ariaDesc="Compliance score"
+                                            ariaTitle="Pie chart compliance"
+                                            constrainToVisibleArea={ true }
+                                            data={ [
+                                                {
+                                                    x: 'Compliant',
+                                                    y: policy.attributes.compliant_host_count
+                                                }, {
+                                                    x: 'Non-compliant',
+                                                    y: policy.attributes.total_host_count - policy.attributes.compliant_host_count
+                                                }
+                                            ] }
+                                            labels={ ({ datum }) => `${datum.x}: ${datum.y}` }
+                                            padding={ pieChartPadding }
+                                            height={ 65 }
+                                            width={ 65 }
+                                            colorScale={ colorScale }
+                                        />
                                     </div>
                                     <div className="ins-c-compliance__row-item">
                                         <Button
+                                            id={ `compliance-link-${index + 1}` }
                                             className="ins-c-compliance__policy-link"
                                             component="a"
-                                            href={ `/${UI_BASE}/compliance/reports/` }
+                                            href={ `/${UI_BASE}/compliance/reports/${policy.id}` }
                                             variant="link"
                                             isInline
                                         >
-                                            {complianceFetchStatus === 'fulfilled' && Array.isArray(complianceSummary.data) &&
-                                                complianceSummary.data.length - 3 >= 1 &&
-                                                    `${complianceSummary.data.length - 3} more compliance reports`
-                                            }
+                                            {policy.attributes.name}
                                         </Button>
+                                        <Split gutter='sm'>
+                                            <SplitItem>
+                                                {intl.formatMessage(messages.compliantHostCount,
+                                                    { count: policy.attributes.total_host_count }
+                                                )}
+                                            </SplitItem>
+                                            <SplitItem>
+                                                {intl.formatMessage(messages.compliantScore,
+                                                    {
+                                                        score: Math.trunc(
+                                                            policy.attributes.compliant_host_count / policy.attributes.total_host_count
+                                                        )
+                                                    }
+                                                )}
+                                            </SplitItem>
+                                        </Split>
+
                                     </div>
                                 </div>
+                            )}
+                            <div className="ins-c-compliance__row">
+                                <div className="ins-c-compliance__row-item">
+                                </div>
+                                <div className="ins-c-compliance__row-item">
+                                    <Button
+                                        className="ins-c-compliance__policy-link"
+                                        component="a"
+                                        href={ `/${UI_BASE}/compliance/reports/` }
+                                        variant="link"
+                                        isInline
+                                    >
+                                        {complianceFetchStatus === 'fulfilled' && Array.isArray(complianceSummary.data) &&
+                                            complianceSummary.data.length - 3 >= 1 &&
+                                            `${complianceSummary.data.length - 3} more compliance reports`
+                                        }
+                                    </Button>
+                                </div>
+                            </div>
 
-                            </React.Fragment> : (
-                                <EmptyState className="ins-c-compliance__empty-state" variant={ EmptyStateVariant.full }>
-                                    <Title headingLevel="h5" size="sm">
-                                        {intl.formatMessage(messages.complianceEmptyStateTitle)}
-                                    </Title>
-                                    <EmptyStateBody>
-                                        {intl.formatMessage(messages.complianceEmptyStateBody)}
-                                    </EmptyStateBody>
-                                    <EmptyStateSecondaryActions>
-                                        <Button
-                                            variant='link'
-                                            href={ `${UI_BASE}/compliance/reports/` }
-                                            component='a'
-                                        >
-                                            {intl.formatMessage(messages.complianceEmptyStateAction1)}
-                                        </Button>
-                                        <Button
-                                            variant='link'
-                                            component='a'
-                                            href="https://www.open-scap.org/getting-started/"
-                                        >
-                                            {intl.formatMessage(messages.complianceEmptyStateAction2)}
-                                        </Button>
-                                    </EmptyStateSecondaryActions>
-                                </EmptyState>
-                            ))
-                        )
-                    }
-                    {complianceFetchStatus === 'pending' && (<Loading />)}
-                    {complianceFetchStatus === 'rejected' && <FailState appName='Compliance'/>}
-                </Stack>
+                        </React.Fragment> : (
+                            <EmptyState className="ins-c-compliance__empty-state" variant={ EmptyStateVariant.full }>
+                                <Title headingLevel="h5" size="sm">
+                                    {intl.formatMessage(messages.complianceEmptyStateTitle)}
+                                </Title>
+                                <EmptyStateBody>
+                                    {intl.formatMessage(messages.complianceEmptyStateBody)}
+                                </EmptyStateBody>
+                                <EmptyStateSecondaryActions>
+                                    <Button
+                                        variant='link'
+                                        href={ `${UI_BASE}/compliance/reports/` }
+                                        component='a'
+                                    >
+                                        {intl.formatMessage(messages.complianceEmptyStateAction1)}
+                                    </Button>
+                                    <Button
+                                        variant='link'
+                                        component='a'
+                                        href="https://www.open-scap.org/getting-started/"
+                                    >
+                                        {intl.formatMessage(messages.complianceEmptyStateAction2)}
+                                    </Button>
+                                </EmptyStateSecondaryActions>
+                            </EmptyState>
+                        ))
+                    )
+                }
+                {complianceFetchStatus === 'pending' && (<Loading />)}
+                {complianceFetchStatus === 'rejected' && <FailState appName='Compliance' />}
             </TemplateCardBody>
         </TemplateCard>
     );
