cb0cbb918f09834bad54f0914bb9947bc6d85e37
Karel Hala <khala@redhat.com>
2022-01-05 11:09:51 +0100

    Use webpack proxy at all times and remove html webpack plugin (#274)

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -2,13 +2,14 @@
 const { resolve } = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const config = require('@redhat-cloud-services/frontend-components-config');
+const HttpsProxyAgent = require('https-proxy-agent');
 const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../'),
     debug: true,
-    appUrl: '/insights/dashboard',
-    betaEnv: 'prod',
+    appUrl: process.env.BETA ? '/beta/insights/dashboard' : '/insights/dashboard',
+    env: `${process.env.ENVIRONMENT || 'stage'}-${process.env.BETA ? 'beta' : 'stable'}`,
     deployment: process.env.BETA ? 'beta/apps' : 'apps',
-    useProxy: process.env.API_ENDOINT ? true : false,
+    useProxy: true,
     localChrome: process.env.INSIGHTS_CHROME,
     customProxy: process.env.API_ENDOINT ? [
         {
@@ -17,7 +18,8 @@ const { config: webpackConfig, plugins } = config({
             secure: true,
             changeOrigin: true,
             autoRewrite: true,
-            ws: true
+            ws: true,
+            ...['prod', 'stage'].includes(process.env.ENVIRONMENT) && { agent: new HttpsProxyAgent('http://squid.corp.redhat.com:3128') }
         }
     ] : []
 });
@@ -28,8 +30,10 @@ plugins.push(
     })
 );
 
+webpackConfig.devServer.client.overlay = false;
+
 module.exports = (env) => {
     env && env.analyze === 'true' && plugins.push(new BundleAnalyzerPlugin());
 
-    return { ...webpackConfig, plugins };
+    return { ...webpackConfig, plugins: plugins.filter((plugin) => plugin.constructor.name !== 'HtmlWebpackPlugin') };
 };
