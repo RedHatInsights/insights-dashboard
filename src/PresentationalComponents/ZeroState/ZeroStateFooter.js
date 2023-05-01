7b478b5041d5bdd2edfed90d60df76e4916820e3
Adonis Puente <60629070+adonispuente@users.noreply.github.com>
2023-05-01 18:29:13 -0400

    feat(RHIN-830):Zero state constants + more dynamic (#370)
    * feat(RHIn-830):Zero state constants + more dynamic
    
    * Add new UX templates

--- src/PresentationalComponents/ZeroState/ZeroStateFooter.js
+++ src/PresentationalComponents/ZeroState/ZeroStateFooter.js
@@ -1,4 +1,4 @@
-import { Title } from '@patternfly/react-core';
+import { Card, PageSection, Title } from '@patternfly/react-core';
 import '../ZeroState/_zero-state.scss';
 
 import {
@@ -18,88 +18,91 @@ const ZeroStateFooter = ({
     documentation = zeroStateConstants[`${appName.toUpperCase()}_ZERO_STATE`].documentation
 }) => {
     return (
-        <div className='footer pf-u-mt-lg pf-u-ml-md pf-u-mr-md pf-u-mb-md'  style={{ backgroundColor: '#fff' }}>
-            <Grid lg={ 4 } hasGutter className='pf-u-p-xl'>
-                <GridItem>
-                    <Flex direction={{ default: 'column' }}>
-                        <FlexItem>
-                            <Title headingLevel='h1' size='lg'>{appName} documentation</Title>
-                        </FlexItem>
-                        {documentation.map(item => (
-                            <FlexItem key={item.title} >
+        <PageSection className='footer pf-u-p-lg'>
+            <Card>
+                <Grid lg={ 4 } hasGutter className='pf-u-p-xl'>
+                    <GridItem>
+                        <Flex direction={{ default: 'column' }}>
+                            <FlexItem>
+                                <Title headingLevel='h1' size='lg'>{appName} documentation</Title>
+                            </FlexItem>
+                            {documentation.map(item => (
+                                <FlexItem key={item.title} >
+                                    <a component='a'
+                                        target='_blank'
+                                        rel="noreferrer"
+                                        href={item.link} >{item.title}</a>
+                                </FlexItem>
+                            ))}
+                        </Flex>
+                    </GridItem>
+                    <GridItem >
+                        <Flex direction={{ default: 'column' }} spacer={{ default: 'spacerSm' }}>
+                            <FlexItem>
+                                <Title headingLevel='h1' size='lg'>Learn about Insights</Title>
+                            </FlexItem>
+                            <FlexItem>
                                 <a component='a'
                                     target='_blank'
-                                    rel="noreferre noreferrer"
-                                    href={item.link} >{item.title}</a>
+                                    rel='noreferrer'
+                                    href={'https://www.redhat.com/en/technologies/management/insights'}>Product page</a>
                             </FlexItem>
-                        ))}
-                    </Flex>
-                </GridItem>
-                <GridItem >
-                    <Flex direction={{ default: 'column' }} spacer={{ default: 'spacerSm' }}>
-                        <FlexItem>
-                            <Title headingLevel='h1' size='lg'>Learn about Insights</Title>
-                        </FlexItem>
-                        <FlexItem>
-                            <a component='a'
-                                target='_blank'
-                                rel='noreferrer'
-                                href={'https://www.redhat.com/en/technologies/management/insights'}>Product page</a>
-                        </FlexItem>
-                        <FlexItem>
-                            <a component='a'
-                                target='_blank'
-                                rel='noreferrer'
-                                href={'https://access.redhat.com/documentation/en-us/red_hat_insights/2023'}>Product documentation</a>
-                        </FlexItem>
-                        <FlexItem>
-                            <a component='a'
-                                target='_blank'
-                                rel='noreferrer' href={'https://www.redhat.com/en/technologies/management/insights/data-application-security'} >
+                            <FlexItem>
+                                <a component='a'
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    href={'https://access.redhat.com/documentation/en-us/red_hat_insights/2023'}>Product documentation</a>
+                            </FlexItem>
+                            <FlexItem>
+                                <a component='a'
+                                    target='_blank'
+                                    rel='noreferrer' href={'https://www.redhat.com/en/technologies/management/insights/data-application-security'} >
                                 Data privacy and controls in Insights
-                            </a>
-                        </FlexItem>
-                        <FlexItem>
-                            <a component='a'
-                                target='_blank'
-                                rel='noreferrer' href={'https://console.redhat.com/docs/api'}>APIs</a>
-                        </FlexItem>
-                        <FlexItem>
-                            <a component='a'
-                                target='_blank'
-                                rel='noreferrer' href={'https://www.redhat.com/en/blog/channel/red-hat-insights'}>Blog</a>
-                        </FlexItem>
-                    </Flex>
-                </GridItem>
-                <GridItem>
-                    <Flex direction={{ default: 'column' }}>
-                        <FlexItem>
-                            <Title headingLevel='h1' size='lg'>Other bundles</Title>
-                        </FlexItem>
-                        <FlexItem>
-                            <a component='a'
-                                target='_blank'
-                                rel='noreferrer' href={'https://console.redhat.com/openshift'}>OpenShift</a>
-                        </FlexItem>
-                        <FlexItem>
-                            <a component='a'
-                                target='_blank'
-                                rel='noreferrer' href={'https://console.redhat.com/application-services/overview'}>Application and Data Services</a>
-                        </FlexItem>
-                        <FlexItem>
-                            <a component='a'
-                                target='_blank'
-                                rel='noreferrer' href={'https://console.redhat.com/edge/'}>Edge Managment</a>
-                        </FlexItem>
-                        <FlexItem>
-                            <a component='a'
-                                target='_blank'
-                                rel='noreferrer' href={'https://console.redhat.com/ansible/ansible-dashboard'}>Ansible Automation Platform</a>
-                        </FlexItem>
-                    </Flex>
-                </GridItem>
-            </Grid>
-        </div>
+                                </a>
+                            </FlexItem>
+                            <FlexItem>
+                                <a component='a'
+                                    target='_blank'
+                                    rel='noreferrer' href={'https://console.redhat.com/docs/api'}>APIs</a>
+                            </FlexItem>
+                            <FlexItem>
+                                <a component='a'
+                                    target='_blank'
+                                    rel='noreferrer' href={'https://www.redhat.com/en/blog/channel/red-hat-insights'}>Blog</a>
+                            </FlexItem>
+                        </Flex>
+                    </GridItem>
+                    <GridItem>
+                        <Flex direction={{ default: 'column' }}>
+                            <FlexItem>
+                                <Title headingLevel='h1' size='lg'>Other bundles</Title>
+                            </FlexItem>
+                            <FlexItem>
+                                <a component='a'
+                                    target='_blank'
+                                    rel='noreferrer' href={'https://console.redhat.com/openshift'}>OpenShift</a>
+                            </FlexItem>
+                            <FlexItem>
+                                <a component='a'
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    href={'https://console.redhat.com/application-services/overview'}>Application and Data Services</a>
+                            </FlexItem>
+                            <FlexItem>
+                                <a component='a'
+                                    target='_blank'
+                                    rel='noreferrer' href={'https://console.redhat.com/edge/'}>Edge Managment</a>
+                            </FlexItem>
+                            <FlexItem>
+                                <a component='a'
+                                    target='_blank'
+                                    rel='noreferrer' href={'https://console.redhat.com/ansible/ansible-dashboard'}>Ansible Automation Platform</a>
+                            </FlexItem>
+                        </Flex>
+                    </GridItem>
+                </Grid>
+            </Card>
+        </PageSection>
 
     );
 };
