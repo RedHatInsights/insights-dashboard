ff49d0ad30a2f9fe1d9dca4c99c85aa7786cf29f
Adonis Puente <adonis.puente@gmail.com>
2024-03-27 17:02:17 -0400

    reimplement api req

--- src/Utilities/useBatchInventoryFetch.js
+++ src/Utilities/useBatchInventoryFetch.js
@@ -1,14 +1,15 @@
 import { useAxiosWithPlatformInterceptors } from '@redhat-cloud-services/frontend-components-utilities/interceptors';
 import { useEffect, useRef, useState } from 'react';
-import { INVENTORY_FETCH_URL, INVENTORY_WARNING_FETCH_URL } from '../AppConstants';
+import { INVENTORY_FETCH_URL, INVENTORY_STALE_FETCH_URL, INVENTORY_WARNING_FETCH_URL } from '../AppConstants';
 import { globalFilters } from './Common';
 
-export const useBatchInventoryFetch = (workloads, SID, selectedTags) => {
+export const useBatchInventoryFetch = (workloads, SID, selectedTags, hasAccess) => {
     const axios = useAxiosWithPlatformInterceptors();
     const mounted = useRef(false);
     const [isLoading, setIsLoading] = useState(true);
     const [inventorySummary, setInventorySummary] = useState();
     const [inventoryWarningSummary, setInventoryWarningSummary] = useState();
+    const [inventoryStaleSum, setInventoryStaleSum] = useState();
     const [error, setError] = useState(false);
 
     useEffect(() => {
@@ -20,11 +21,15 @@ export const useBatchInventoryFetch = (workloads, SID, selectedTags) => {
                 setIsLoading(true);
                 const inventorySum = options !== 'undefined' && await axios.get(`${INVENTORY_FETCH_URL}`, { params: { ...options } });
                 const inventoryWarningSum = options !== 'undefined' && await axios.get(`${INVENTORY_WARNING_FETCH_URL}`, { params: { ...options } });
+                const inventoryStaleSum = options !== 'undefined' && await axios.get(`${INVENTORY_STALE_FETCH_URL}`, { params: { ...options } });
+
                 mounted.current &&
                     (
-                        setIsLoading(false),
                         setInventorySummary(inventorySum),
-                        setInventoryWarningSummary(inventoryWarningSum)
+                        setInventoryWarningSummary(inventoryWarningSum),
+                        setInventoryStaleSum(inventoryStaleSum),
+                        setIsLoading(false)
+
                     );
             } catch (err) {
                 console.error(err);
@@ -32,11 +37,16 @@ export const useBatchInventoryFetch = (workloads, SID, selectedTags) => {
             }
         };
 
-        fetchData();
+        hasAccess === true && fetchData();
         return () => {
             mounted.current = false;
         };
-    }, [workloads, SID, selectedTags]);
+    }, [workloads, SID, selectedTags, hasAccess]);
 
-    return [isLoading, inventorySummary, inventoryWarningSummary, error];
+    return [
+        isLoading,
+        inventorySummary,
+        inventoryWarningSummary,
+        inventoryStaleSum,
+        error];
 };
