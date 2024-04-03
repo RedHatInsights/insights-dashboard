94a8d786d4518b645521a948be26f25060a3bbb7
Adonis Puente <adonis.puente@gmail.com>
2024-04-03 11:06:04 -0400

    update test and remove boolean error

--- config/overrideChrome.js
+++ config/overrideChrome.js
@@ -6,5 +6,24 @@ export default () => ({
     on: () => {},
     getApp: () => 'dashboard',
     getBundle: () => 'insights',
-    getUserPermissions: () => [{ permission: 'inventory:*:*' }]
+    getUserPermissions: () => [{ permission: 'inventory:*:*' }],
+    analytics: {
+        track: () => fetch('/analytics/track')
+    },
+    auth: {
+        getUser: () => Promise.resolve(true),
+        logout: () => Promise.resolve(true)
+    }
+});
+
+export const useChrome = () => ({
+    updateDocumentTitle: () => undefined,
+    isBeta: () => false,
+    analytics: {
+        track: () => fetch('/analytics/track')
+    },
+    auth: {
+        getUser: () => Promise.resolve(true),
+        logout: () => Promise.resolve(true)
+    }
 });
