6a23551818f55516a7862b14957f7febd29df51b
Sebastian Gräßl <mail@bastilian.me>
2024-06-21 16:10:01 +0200

    fix(App): Store store in const
    The store appears to change everytime the App is rendered this will prevent the store from changing in these cases.

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -1,16 +1,21 @@
 /* eslint-disable no-console */
+import React from 'react';
 import App from './App';
 import IntlProvider from '@redhat-cloud-services/frontend-components-translations/Provider';
 import { Provider } from 'react-redux';
-import React from 'react';
 import { init } from './Store';
 import messages from '../locales/data.json';
 
-const AppEntry = () =>
-    <IntlProvider locale={ navigator.language.slice(0, 2) } messages={ messages } onError={ console.log }>
-        <Provider store={ init().getStore() }>
+const store = init().getStore();
+const locale = navigator.language.slice(0, 2);
+
+const AppEntry = () => (
+
+    <IntlProvider locale={ locale } messages={ messages }>
+        <Provider store={ store }>
             <App />
         </Provider>
-    </IntlProvider>;
+    </IntlProvider>
+);
 
 export default AppEntry;
