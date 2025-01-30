47b9bbfb26bcdf79c66fc97e5d5484c2fa61a5cb
Adonis Puente <60629070+adonispuente@users.noreply.github.com>
2025-01-30 08:49:30 -0600

    feat(RHINENG-15382): Onboard Dashboard to Sentry (#564)

--- fec.config.js
+++ fec.config.js
@@ -1,12 +1,33 @@
 const { resolve } = require('path');
 const packageJson = require('./package.json');
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 
 module.exports = {
     appUrl: '/insights/dashboard',
     debug: true,
     useProxy: process.env.PROXY === 'true',
     proxyVerbose: true,
-    plugins: [],
+    devtool: 'hidden-source-map',
+    plugins: [
+        // Put the Sentry Webpack plugin after all other plugins
+        ...(process.env.ENABLE_SENTRY
+            ? [
+                sentryWebpackPlugin({
+                    ...(process.env.SENTRY_AUTH_TOKEN && {
+                        authToken: process.env.SENTRY_AUTH_TOKEN
+                    }),
+                    org: 'red-hat-it',
+                    project: 'dashboard-rhel',
+                    moduleMetadata: ({ release }) => ({
+                        dsn: `https://f8eb44de949e487e853185c09340f3cf@o490301.ingest.us.sentry.io/4505397435367424`,
+                        org: 'red-hat-it',
+                        project: 'dashboard-rhel',
+                        release
+                    })
+                })
+            ]
+            : [])
+    ],
     ...(process.env.port ? { port: parseInt(process.env.port) } : {}),
     moduleFederation: {
         shared: [
