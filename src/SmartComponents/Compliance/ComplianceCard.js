f02e0ee424c845571079839bab8c2cd0c70af747
zhamilya <@computercamplove>
2026-03-12 17:05:25 +0100

    fix: Remove global tag filtering form compliance card

--- src/SmartComponents/Compliance/ComplianceCard.js
+++ src/SmartComponents/Compliance/ComplianceCard.js
@@ -20,19 +20,16 @@ import {
 } from '@patternfly/react-core';
 import React, { useEffect } from 'react';
 import { TemplateCardActions, TemplateCardBody } from '../../PresentationalComponents/Template/TemplateCard';
-import { supportsGlobalFilter, workloadsPropType } from '../../Utilities/Common';
 
 import { ExpandableCardTemplate } from '../../PresentationalComponents/Template/ExpandableCardTemplate';
 import FailState from '../../PresentationalComponents/FailState/FailState';
-import FilterNotSupported from '../../PresentationalComponents/FilterNotSupported';
 import PropTypes from 'prop-types';
-import { UI_BASE } from '../../AppConstants';
 import { connect } from 'react-redux';
 import messages from '../../Messages';
 import { useIntl } from 'react-intl';
 import InsightsLink from '@redhat-cloud-services/frontend-components/InsightsLink';
 
-const ComplianceCard = ({ fetchCompliance, complianceFetchStatus, complianceSummary, selectedTags, workloads }) => {
+const ComplianceCard = ({ fetchCompliance, complianceFetchStatus, complianceSummary }) => {
     useEffect(() => {
         fetchCompliance();
     }, [fetchCompliance]);
@@ -65,95 +62,88 @@ const ComplianceCard = ({ fetchCompliance, complianceFetchStatus, complianceSumm
             }
             body={
                 <TemplateCardBody>
-                    {supportsGlobalFilter(selectedTags, workloads) ?
-                        <React.Fragment>
-                            {complianceFetchStatus === 'fulfilled' &&
-                                (Array.isArray(complianceSummary.data) &&
-                                    (complianceSummary.data.length > 0 ? <React.Fragment>
-                                        <DataList className='insd-m-no-padding insd-m-no-top-border' isCompact>
-                                            {getTopThreeReports(complianceSummary).map((report, index) =>
-                                                <DataListItem key={ index }>
-                                                    <DataListItemRow>
-                                                        <DataListItemCells
-                                                            dataListCells={[
-                                                                <DataListCell key={ index }>
-                                                                    <Flex
-                                                                        direction={ { default: 'column' } }
-                                                                        spaceItems={ { default: 'spaceItemsNone' } }
+                    <React.Fragment>
+                        {complianceFetchStatus === 'fulfilled' &&
+                            (Array.isArray(complianceSummary.data) &&
+                                (complianceSummary.data.length > 0 ? <React.Fragment>
+                                    <DataList className='insd-m-no-padding insd-m-no-top-border' isCompact>
+                                        {getTopThreeReports(complianceSummary).map((report, index) =>
+                                            <DataListItem key={ index }>
+                                                <DataListItemRow>
+                                                    <DataListItemCells
+                                                        dataListCells={[
+                                                            <DataListCell key={ index }>
+                                                                <Flex
+                                                                    direction={ { default: 'column' } }
+                                                                    spaceItems={ { default: 'spaceItemsNone' } }
+                                                                >
+                                                                    <InsightsLink
+                                                                        app='compliance'
+                                                                        to={`/reports/${report.id}`}
+                                                                        className='ins-c-compliance__policy-link
+                                                                        pf-v6-c-button pf-m-link pf-m-inline'
+                                                                        id={ `compliance-link-${index + 1}` }
                                                                     >
-                                                                        <InsightsLink
-                                                                            app='compliance'
-                                                                            to={`/reports/${report.id}`}
-                                                                            className='ins-c-compliance__policy-link
-                                                                            pf-v6-c-button pf-m-link pf-m-inline'
-                                                                            id={ `compliance-link-${index + 1}` }
-                                                                        >
-                                                                            {report.title}
-                                                                        </InsightsLink>
-                                                                        <Flex>
-                                                                            <FlexItem flex={ { default: 'flex_1' } }>
-                                                                                <p>RHEL { report.os_major_version }</p>
+                                                                        {report.title}
+                                                                    </InsightsLink>
+                                                                    <Flex>
+                                                                        <FlexItem flex={ { default: 'flex_1' } }>
+                                                                            <p>RHEL { report.os_major_version }</p>
+                                                                        </FlexItem>
+                                                                        <Flex flex={{ default: 'None' }}>
+                                                                            <FlexItem>
+                                                                                {intl.formatMessage(messages.compliantHostCount,
+                                                                                    { count: report.reported_system_count }
+                                                                                )}
                                                                             </FlexItem>
-                                                                            <Flex flex={{ default: 'None' }}>
-                                                                                <FlexItem>
-                                                                                    {intl.formatMessage(messages.compliantHostCount,
-                                                                                        { count: report.reported_system_count }
-                                                                                    )}
-                                                                                </FlexItem>
-                                                                                <FlexItem>
-                                                                                    {intl.formatMessage(messages.compliantScore,
-                                                                                        { score: report.percent_compliant }
-                                                                                    )}
-                                                                                </FlexItem>
-                                                                            </Flex>
+                                                                            <FlexItem>
+                                                                                {intl.formatMessage(messages.compliantScore,
+                                                                                    { score: report.percent_compliant }
+                                                                                )}
+                                                                            </FlexItem>
                                                                         </Flex>
                                                                     </Flex>
-                                                                </DataListCell>
-                                                            ]}
-                                                        />
-                                                    </DataListItemRow>
-                                                </DataListItem>
-                                            )}
-                                        </DataList>
-                                        <InsightsLink app='compliance' to='/reports' className='pf-v6-c-button pf-m-link pf-m-inline'>
-                                            {complianceFetchStatus === 'fulfilled' && Array.isArray(complianceSummary.data) &&
-                                                complianceSummary.data.length - 3 >= 1 &&
-                                                `${complianceSummary.data.length - 3} more reports`
-                                            }
-                                        </InsightsLink>
-                                    </React.Fragment> : (
-                                        <EmptyState className='ins-c-compliance__empty-state' variant={ EmptyStateVariant.full }>
-                                            <Title headingLevel='h5' size='md'>
-                                                {intl.formatMessage(messages.complianceEmptyStateTitle)}
-                                            </Title>
-                                            <EmptyStateBody>
-                                                {intl.formatMessage(messages.complianceEmptyStateBody)}
-                                            </EmptyStateBody>
-                                            <EmptyStateActions>
-                                                <InsightsLink app='compliance' to='/scappolicies/new' className='pf-v6-c-button pf-m-link'>
-                                                    {intl.formatMessage(messages.complianceEmptyStateAction1)}
-                                                </InsightsLink>
-                                                <Button
-                                                    variant='link'
-                                                    component='a'
-                                                    href='https://www.open-scap.org/getting-started/'
-                                                >
-                                                    {intl.formatMessage(messages.complianceEmptyStateAction2)}
-                                                </Button>
-                                            </EmptyStateActions>
-                                        </EmptyState>
-                                    ))
-                                )
-                            }
-                            {complianceFetchStatus === 'pending' && (<Spinner />)}
-                            {complianceFetchStatus === 'rejected' && <FailState appName='Compliance' />}
-                        </React.Fragment>
-                        : <FilterNotSupported
-                            href={ `${UI_BASE}/compliance/` }
-                            title={ intl.formatMessage(messages.filterNotApplicable) }
-                            appName={ intl.formatMessage(messages.complianceTitle) }
-                        />
-                    }
+                                                                </Flex>
+                                                            </DataListCell>
+                                                        ]}
+                                                    />
+                                                </DataListItemRow>
+                                            </DataListItem>
+                                        )}
+                                    </DataList>
+                                    <InsightsLink app='compliance' to='/reports' className='pf-v6-c-button pf-m-link pf-m-inline'>
+                                        {complianceFetchStatus === 'fulfilled' && Array.isArray(complianceSummary.data) &&
+                                            complianceSummary.data.length - 3 >= 1 &&
+                                            `${complianceSummary.data.length - 3} more reports`
+                                        }
+                                    </InsightsLink>
+                                </React.Fragment> : (
+                                    <EmptyState className='ins-c-compliance__empty-state' variant={ EmptyStateVariant.full }>
+                                        <Title headingLevel='h5' size='md'>
+                                            {intl.formatMessage(messages.complianceEmptyStateTitle)}
+                                        </Title>
+                                        <EmptyStateBody>
+                                            {intl.formatMessage(messages.complianceEmptyStateBody)}
+                                        </EmptyStateBody>
+                                        <EmptyStateActions>
+                                            <InsightsLink app='compliance' to='/scappolicies/new' className='pf-v6-c-button pf-m-link'>
+                                                {intl.formatMessage(messages.complianceEmptyStateAction1)}
+                                            </InsightsLink>
+                                            <Button
+                                                variant='link'
+                                                component='a'
+                                                href='https://www.open-scap.org/getting-started/'
+                                            >
+                                                {intl.formatMessage(messages.complianceEmptyStateAction2)}
+                                            </Button>
+                                        </EmptyStateActions>
+                                    </EmptyState>
+                                ))
+                            )
+                        }
+                        {complianceFetchStatus === 'pending' && (<Spinner />)}
+                        {complianceFetchStatus === 'rejected' && <FailState appName='Compliance' />}
+                    </React.Fragment>
                 </TemplateCardBody>
             }
         />
@@ -164,16 +154,12 @@ ComplianceCard.propTypes = {
     fetchCompliance: PropTypes.func,
     complianceSummary: PropTypes.object,
     complianceFetchStatus: PropTypes.string,
-    intl: PropTypes.any,
-    selectedTags: PropTypes.array,
-    workloads: workloadsPropType
+    intl: PropTypes.any
 };
 
 const mapStateToProps = ({ DashboardStore }) => ({
     complianceSummary: DashboardStore.complianceSummary,
-    complianceFetchStatus: DashboardStore.complianceFetchStatus,
-    selectedTags: DashboardStore.selectedTags,
-    workloads: DashboardStore.workloads
+    complianceFetchStatus: DashboardStore.complianceFetchStatus
 });
 
 const mapDispatchToProps = dispatch => ({
